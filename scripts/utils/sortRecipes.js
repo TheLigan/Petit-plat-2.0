829926b9fcc9cc606b0796460fb8f8cc93ed45b9
Verdon Xavier <verdonxavier2@gmail.com>
2023-08-14 02:21:31 +0200

    1.1 corrections

--- scripts/utils/sortRecipes.js
+++ scripts/utils/sortRecipes.js
@@ -9,13 +9,13 @@ let resultEvent;
 let searchRecipe;
 let indexRecipe;
 const accent = [
-    /[\300-\306]/g, /[\340-\346]/g, 
-    /[\310-\313]/g, /[\350-\353]/g, 
-    /[\314-\317]/g, /[\354-\357]/g, 
-    /[\322-\330]/g, /[\362-\370]/g, 
-    /[\331-\334]/g, /[\371-\374]/g,
-    /[\321]/g, /[\361]/g, 
-    /[\307]/g, /[\347]/g, 
+    /[\300-\306]/g, /[\340-\346]/g, // A, a
+    /[\310-\313]/g, /[\350-\353]/g, // E, e
+    /[\314-\317]/g, /[\354-\357]/g, // I, i
+    /[\322-\330]/g, /[\362-\370]/g, // O, o
+    /[\331-\334]/g, /[\371-\374]/g, // U, u
+    /[\321]/g, /[\361]/g, // N, n
+    /[\307]/g, /[\347]/g, // C, c
 ];
 const noAccent = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
 
@@ -34,15 +34,15 @@ const listOptionsTag = document.querySelectorAll(".list-options-tag span");
 const noRecipeFound = document.querySelector(".no-recipe-found");
 
 
-
+ 
 controlText.addEventListener("input", () => {
     displayRecipes();
-});
+}); 
 
 headerRecipeTag.forEach((header, index) => header.addEventListener("click", () => {
     indexTag = index;
     displayHideOptionsOfTag(indexTag)
-})); 
+}));  
 listOptionsTag.forEach((option) => option.addEventListener("click", (event) => {
 
     indexTag = arrOptionsTag.indexOf(option.parentElement);
@@ -50,11 +50,11 @@ listOptionsTag.forEach((option) => option.addEventListener("click", (event) => {
 
     if (arrTag.length == 0) {
         createTag(valueOption);
-        arrTag.push(valueOption);
-        displayRecipes();
+        arrTag.push(valueOption); 
+        displayRecipes(); 
         displayHideOptionsOfTag(indexTag);
     }
-    else {
+    else { 
         let countTag = 0;
         for (let tag of arrTag) {
             if (tag == valueOption) {
@@ -63,8 +63,8 @@ listOptionsTag.forEach((option) => option.addEventListener("click", (event) => {
         }
         if (countTag == 0) {
             createTag(valueOption);
-            arrTag.push(valueOption);
-            displayRecipes();
+            arrTag.push(valueOption);  
+            displayRecipes(); 
             displayHideOptionsOfTag(indexTag);
         }
     }
@@ -72,13 +72,13 @@ listOptionsTag.forEach((option) => option.addEventListener("click", (event) => {
 }));
 controlTextRecipeTag.forEach((text, index) => text.addEventListener("input", () => {
     displayOptions(index);
-}));
+})); 
 
 
+ 
+function displayHideOptionsOfTag(index) { 
 
-function displayHideOptionsOfTag(index) {
-
-    if (searchTag[index].style.display == "block" && optionsTag[index].style.display == "flex") {
+    if (searchTag[index].style.display == "block" && optionsTag[index].style.display == "flex") { 
         searchTag[index].style.display = "none";
         optionsTag[index].style.display = "none";
         iconRecipeTag[index].classList.remove("fa-rotate-180");
@@ -87,7 +87,7 @@ function displayHideOptionsOfTag(index) {
             option.style.display = "block";
         }
     }
-    else {
+    else { 
         searchTag[index].style.display = "block";
         controlTextRecipeTag[index].focus();
         optionsTag[index].style.display = "flex";
@@ -98,14 +98,14 @@ function displayHideOptionsOfTag(index) {
         }
     }
 }
-function displayOptions(index) {
+function displayOptions(index) { 
 
-    let textSearchUser = removeAccentText(controlTextRecipeTag[index].value);
-    let regexSearch = new RegExp(textSearchUser);;
+    let textSearchUser = removeAccentText(controlTextRecipeTag[index].value); 
+    let regexSearch = new RegExp(textSearchUser);; 
     let resultSearch = [];
     let optionTag;
 
-    
+     
     for (let option of optionsTag[index].childNodes) {
 
         optionTag = removeAccentText(option.textContent);
@@ -115,7 +115,7 @@ function displayOptions(index) {
         }
 
     }
-    
+     
     for (let option of optionsTag[index].childNodes) {
         option.style.display = "none";
         for (let result of resultSearch) {
@@ -126,7 +126,7 @@ function displayOptions(index) {
     }
 
 }
-function controlSearchBar() {
+function controlSearchBar() { 
 
     if (controlText.value == "") {
         searchBar.dataset.errorVisible = "true";
@@ -146,77 +146,88 @@ function controlSearchBar() {
     }
     return { resultEvent, searchRecipe };
 }
-function removeAccentText(text) {
-    strText = text.toLowerCase(); 
+function removeAccentText(text) { 
+    strText = text.toLowerCase();  
     for (let i = 0; i < accent.length; i++) {
         strText = strText.replace(accent[i], noAccent[i]);
     }
 
     return strText;
 }
-function filterBySearchBar() { 
+function filterBySearchBar() {  
 
-    if (controlSearchBar().resultEvent == "true") {
+    if (controlSearchBar()["resultEvent"] == "true") {
 
-        let textSearchUser = removeAccentText(controlSearchBar().searchRecipe);
-        let arrTextSearchUser = textSearchUser.split(' '); 
-        let regexSearch;
+        let textSearchUser = removeAccentText(controlSearchBar()["searchRecipe"]); 
+        let arrTextSearchUser = textSearchUser.split(' ');  
+        let regexSearch; 
         let resultSearch = [];
-
-       
-        for (let indexRecipe = 0; indexRecipe < recipes.length; indexRecipe++) {
-
-            for (let searchUser of arrTextSearchUser) {
+        let resultTitle;
+        let resultAppliance;
+        let resultIngredient;
+        let resultUstensils;
+      
+        recipes.forEach((recipe, index) => {
+            arrTextSearchUser.forEach((searchUser) => {
                 regexSearch = new RegExp(searchUser);
 
-                
-                let titRecipe = removeAccentText(recipes[indexRecipe].name);
-
-                if (titRecipe.match(regexSearch) !== null && titRecipe.match(regexSearch)[0].length > 2) {
-                   
-                    resultSearch.push(listCardRecipes[indexRecipe]);
-                }
-
-                
-                const listIngredientsRecipe = recipes[indexRecipe].ingredients;
-                for (let ingredient of listIngredientsRecipe) {
-                    let ingrRecipe = removeAccentText(ingredient.ingredient);
-
-                    if (ingrRecipe.match(regexSearch) !== null && ingrRecipe.match(regexSearch)[0].length > 2) {
-                       
-                        resultSearch.push(listCardRecipes[indexRecipe]);
+                 
+                let titRecipe = removeAccentText(recipe["name"]);
+                resultTitle = recipes.filter((recipe) => matchRecipes(recipe, titRecipe, regexSearch));
+                resultTitle.forEach((retitle) => {
+                    if (recipe == retitle) {
+                        resultSearch.push(listCardRecipes[index]);
                     }
-                }
-        
-               
-                let appliRecipe = removeAccentText(recipes[indexRecipe].appliance);
-
-                if (appliRecipe.match(regexSearch) !== null && appliRecipe.match(regexSearch)[0].length > 2) {
-                   
-                    resultSearch.push(listCardRecipes[indexRecipe]);
-                }
-        
-               
-                const listUstensilsRecipe = recipes[indexRecipe].ustensils;
-                for (let ustensils of listUstensilsRecipe) {
+                });
+                 
+                let appliRecipe = removeAccentText(recipe["appliance"]);
+                resultAppliance = recipes.filter((recipe) => matchRecipes(recipe, appliRecipe, regexSearch));
+                resultAppliance.forEach((reAppl) => {
+                    if (recipe == reAppl) {
+                        resultSearch.push(listCardRecipes[index]);
+                    }
+                });
+                 
+                const listIngredientsRecipe = recipe["ingredients"];
+                listIngredientsRecipe.forEach((ingredient) => {
+                    let ingrRecipe = removeAccentText(ingredient["ingredient"]);
+                    resultIngredient = recipes.filter((recipe) => matchRecipes(recipe, ingrRecipe, regexSearch));
+                    resultIngredient.forEach((reIngr) => {
+                        if (recipe == reIngr) {
+                            resultSearch.push(listCardRecipes[index]);
+                        }
+                    });
+                });
+                 
+                const listUstensilsRecipe = recipe["ustensils"];
+                listUstensilsRecipe.forEach((ustensils) => {
                     let ustenRecipe = removeAccentText(ustensils);
+                    resultUstensils = recipes.filter((recipe) => matchRecipes(recipe, ustenRecipe, regexSearch));
+                    resultUstensils.forEach((reUsten) => {
+                        if (recipe == reUsten) {
+                            resultSearch.push(listCardRecipes[index]);
+                        }
+                    });
+                });
+            });
+        })
 
-                    if (ustenRecipe.match(regexSearch) !== null && ustenRecipe.match(regexSearch)[0].length > 2) {
-                       
-                        resultSearch.push(listCardRecipes[indexRecipe]);
-                    }
-                }
-            }
-        }
         return { resultSearch, textSearchUser };
     }
     else {
         controlSearchBar();
     }
 }
-function createTag(value) {
+function matchRecipes(recipe, nRecipe, regexSearch) { 
+
+    if (nRecipe.match(regexSearch) !== null && nRecipe.match(regexSearch)[0].length > 2) {
+         
+        return recipe;
+    }
+}
+function createTag(value) { 
     if (value !== undefined) {
-        
+         
         const divTagOption = document.createElement('div');
         divTagOption.className = "recipes-filter__tag__option";
         const icon_Close = document.createElement('i');
@@ -227,27 +238,27 @@ function createTag(value) {
         divTagOption.appendChild(tag_Option);
         divTagOption.appendChild(icon_Close);
 
-        
+         
         icon_Close.addEventListener("click", () => {
-            icon_Close.parentElement.remove();
+            icon_Close.parentElement.remove(); 
             let valueOptionOfIcon = removeAccentText(icon_Close.parentElement.querySelector('.recipes-filter__tag__option span').textContent);
-            arrTag = arrTag.filter((tag) => removeAccentText(tag) !== valueOptionOfIcon);
-            displayRecipes();
+            arrTag = arrTag.filter((tag) => removeAccentText(tag) !== valueOptionOfIcon); 
+            displayRecipes(); 
         });
     }
 }
-function filterByTag() {
+function filterByTag() { 
     if (arrTag !== undefined) {
         let resultSearch = [];
 
         for (let tag of arrTag) {
 
-            let valueTag = removeAccentText(tag);
+            let valueTag = removeAccentText(tag);  
 
-            
+             
             for (let indexRecipe = 0; indexRecipe < recipes.length; indexRecipe++) {
 
-                
+                 
                 const listIngredientsRecipe = recipes[indexRecipe]["ingredients"];
                 for (let ingredient of listIngredientsRecipe) {
                     let ingrRecipe = removeAccentText(ingredient["ingredient"]);
@@ -257,7 +268,7 @@ function filterByTag() {
                     }
                 }
 
-                
+                 
                 const applianceRecipe = removeAccentText(recipes[indexRecipe].appliance);
                 if (applianceRecipe === valueTag) {
                     resultSearch.push(listCardRecipes[indexRecipe]);
@@ -319,7 +330,7 @@ function resultSearchUser() {
                             countTag += 1;
                         }
 
-                     
+                        
                         for (ustensil of listUstensilsRecipe) {
                             let ustensilRecipe = removeAccentText(ustensil);
 
@@ -329,7 +340,7 @@ function resultSearchUser() {
                         }
                     }
 
-                 
+                    
                     if (countTag == arrTag.length && arrTag.length !== 0) {
                         resultTags.push(result);
                     }
@@ -352,7 +363,7 @@ function resultSearchUser() {
 
     return { resultSearchBar, resultTags, resultGlobal };
 }
-function displayRecipes() { 
+function displayRecipes() {
 
     let countRecipes = new Number();
 
@@ -364,7 +375,7 @@ function displayRecipes() {
 
         for (let recipe of listCardRecipes) {
             recipe.style.display = "none";
-            recipe.classList.remove("fade-in-recipe"); 
+            recipe.classList.remove("fade-in-recipe");
 
             for (let i = 0; i < resultSearchBar.length; i++) {
                 if (recipe === resultSearchBar[i]) {
@@ -375,10 +386,10 @@ function displayRecipes() {
                 }
             }
         }
-         
+        
         countRecipes = [...new Set(resultSearchBar)].length;
     }
-    else if (arrTag.length !== 0 && controlText.value == "") { 
+    else if (arrTag.length !== 0 && controlText.value == "") {
 
         let resultTags = [...new Set(resultSearchUser().resultTags)];
         let baseDelay = 0.2;
@@ -391,52 +402,52 @@ function displayRecipes() {
                 if (recipe === resultTags[i]) {
                     recipe.style.display = "block";
                     resultTags[i].style.setProperty("--fadeinDelayRecipe", baseDelay + ((i + 1) / 5) + "s");
-                    recipe.offsetWidth; 
-                    recipe.classList.add("fade-in-recipe");  
+                    recipe.offsetWidth;
+                    recipe.classList.add("fade-in-recipe"); 
                 }
             }
         }
-         
+        
         countRecipes = [...new Set(resultTags)].length;
     }
-    else if (arrTag.length !== 0 && controlText.value !== "") { 
+    else if (arrTag.length !== 0 && controlText.value !== "") {
 
         let resultGlobal = [...new Set(resultSearchUser().resultGlobal)];
         let baseDelay = 0.2;
 
         for (let recipe of listCardRecipes) {
             recipe.style.display = "none";
-            recipe.classList.remove("fade-in-recipe"); 
+            recipe.classList.remove("fade-in-recipe");
 
             for (let i = 0; i < resultGlobal.length; i++) {
                 if (recipe === resultGlobal[i]) {
                     recipe.style.display = "block";
                     resultGlobal[i].style.setProperty("--fadeinDelayRecipe", baseDelay + ((i + 1) / 5) + "s");
-                    recipe.offsetWidth; 
-                    recipe.classList.add("fade-in-recipe");  
+                    recipe.offsetWidth;
+                    recipe.classList.add("fade-in-recipe"); 
                 }
             }
         }
-         
+        
         countRecipes = [...new Set(resultGlobal)].length;
     }
     else {
         let baseDelay = 0.2;
 
         for (let i = 0; i < listCardRecipes.length; i++) {
-            listCardRecipes[i].classList.remove("fade-in-recipe"); 
+            listCardRecipes[i].classList.remove("fade-in-recipe");
             listCardRecipes[i].style.display = "block";
             listCardRecipes[i].style.setProperty("--fadeinDelayRecipe", baseDelay + ((i + 1) / 5) + "s");
-            listCardRecipes[i].offsetWidth;  
-            listCardRecipes[i].classList.add("fade-in-recipe");  
+            listCardRecipes[i].offsetWidth; 
+            listCardRecipes[i].classList.add("fade-in-recipe"); 
         }
-         
+        
         countRecipes = listCardRecipes.length;
     }
-     
+    
     totRecipes.textContent = countRecipes + " recettes";
 
-     
+    
     if (countRecipes == 0) {
         noRecipeFound.style.display = "block";
     }
